64d28a382a06a932a4ffe7b7457c467cde89dab9
Leandro <leoliveira1911@gmail.com>
2023-05-28 12:07:11 -0300

    all pages working on new db

--- src/pages/Ranking.tsx
+++ src/pages/Ranking.tsx
@@ -1,35 +1,36 @@
-import axios from 'axios'
-import React, {useEffect, useState} from 'react'
-import Category from '../components/Category'
-import Layout from '../components/Layout'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import Category from "../components/Category";
+import Layout from "../components/Layout";
 
 export default function Ranking() {
-	useEffect(() => {
-		getRanking()
-	}, [])
+  useEffect(() => {
+    getRanking();
+  }, []);
 
-	const [ranking, setRanking] = useState<[{user: string; correct: number}]>([
-		{user: '', correct: 0},
-	])
+  const [ranking, setRanking] = useState<[{ user: string; correct: number }]>([
+    { user: "", correct: 0 },
+  ]);
 
-	function renderRanking(ranking: [{user: string; correct: number}]) {
-		return ranking.map(user => {
-			return (
-				<Category
-					title={`${ranking.indexOf(user) + 1}º Colocado: ${user.user}`}>
-					<h1>Acertos: {user.correct} / 23</h1>
-				</Category>
-			)
-		})
-	}
+  function renderRanking(ranking: [{ user: string; correct: number }]) {
+    return ranking.map((user) => {
+      return (
+        <Category
+          title={`${ranking.indexOf(user) + 1}º Colocado: ${user.user}`}
+        >
+          <h1>Acertos: {user.correct} / 23</h1>
+        </Category>
+      );
+    });
+  }
 
-	function getRanking() {
-		axios
-			.get('https://crazy-duck-baseball-cap.cyclic.app/api/getRanking')
-			.then(data => data.data)
-			.then(setRanking)
-		// .then(data => setRanking(data.data))
-	}
+  function getRanking() {
+    axios
+      .get("http://localhost:3002/getRanking")
+      .then((data) => data.data)
+      .then(setRanking);
+    // .then(data => setRanking(data.data))
+  }
 
-	return <Layout title="Ranking">{renderRanking(ranking)}</Layout>
+  return <Layout title="Ranking">{renderRanking(ranking)}</Layout>;
 }
